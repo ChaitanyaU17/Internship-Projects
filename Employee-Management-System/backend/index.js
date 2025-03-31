58748293513f6e5cebb6eef13ea7021a9663dd71
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-03-31 23:35:57 +0530

    add db in index.js file

--- Employee-Management-System/backend/index.js
+++ Employee-Management-System/backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
+require('./Models/db');
 
 app.get('/', (req, res) => {
     res.send("Employee management server is running..")
