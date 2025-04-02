bf91c2781a4ff9dc2b60d9ee837a6b89935d65c9
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-04-02 12:10:48 +0530

    created routes for employee controller post method

---
+++ Employee-Management-System/backend/Routes/EmployeeRoutes.js
@@ -0,0 +1,12 @@
+const {createEmployee} = require("../Controllers/EmployeeController");
+const { cloudinaryFileUploader } = require("../Middlewares/FileUploader");
+
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+    res.send('Get all Employees');
+})
+
+routes.post('/', cloudinaryFileUploader.single('profileImage'), createEmployee);
+
+module.exports = routes;
