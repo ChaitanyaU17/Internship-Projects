a3bc3d6029f5f2c657fd05e6016f3269e7433a18
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-04-03 18:05:56 +0530

    give routes for employees by id and import all to the routes file

--- Employee-Management-System/backend/Routes/EmployeeRoutes.js
+++ Employee-Management-System/backend/Routes/EmployeeRoutes.js
@@ -1,12 +1,12 @@
-const {createEmployee} = require("../Controllers/EmployeeController");
+const {createEmployee, getAllEmployees, getEmployeeById} = require("../Controllers/EmployeeController");
 const { cloudinaryFileUploader } = require("../Middlewares/FileUploader");
 
 const routes = require('express').Router();
 
-routes.get('/', (req, res) => {
-    res.send('Get all Employees');
-})
+routes.get('/', getAllEmployees);
 
 routes.post('/', cloudinaryFileUploader.single('profileImage'), createEmployee);
+
+routes.get('/:id', getEmployeeById);
 
 module.exports = routes;
