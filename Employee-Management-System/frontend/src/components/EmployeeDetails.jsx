fd83bfeef31be9f130ee1dab605f24d928180948
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-04-06 12:16:02 +0530

    createdemployeedetails and employeemanagementapp page

---
+++ Employee-Management-System/frontend/src/components/EmployeeDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeDetails = () => {
+  return (
+    <div>
+      Employee details page
+    </div>
+  )
+}
+
+export default EmployeeDetails;
---
+++ Employee-Management-System/frontend/src/components/EmployeeManagementApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeManagementApp = () => {
+  return (
+    <div>
+      EmployeeManagementApp
+    </div>
+  )
+}
+
+export default EmployeeManagementApp;
