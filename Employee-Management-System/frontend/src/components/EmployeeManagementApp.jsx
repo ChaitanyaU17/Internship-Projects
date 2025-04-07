6111453aad7ede1c6fe6019cc7c0fe977ff2e19e
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-04-07 19:04:04 +0530

    created employee management app  where state variable stores employees data and pagination info

--- Employee-Management-System/frontend/src/components/EmployeeManagementApp.jsx
+++ Employee-Management-System/frontend/src/components/EmployeeManagementApp.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import EmployeeTable from "./EmployeeTable";
+import { GetAllEmployess } from "../api";
 
 const EmployeeManagementApp = () => {
+  const [employeeData, setEmployeeData] = useState({
+    employees: [],
+    pagination: {
+      totalEmployees: 0,
+      currentPage: 1,
+      totalPages: 1,
+      pageSize: 5,
+    },
+  });
+  const fetchEmployees = async (search = "", page = 1, limit = 5) => {
+    try {
+      const { data } = await GetAllEmployess(search, page, limit);
+      setEmployeeData(data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
   return (
-    <div>
-      EmployeeManagementApp
+    <div className="d-flex flex-column justify-content-center align-items-center w-100 p-3">
+      <h1>Employee Management App</h1>
+      <div className="w-100 d-flex justify-content-center">
+        <div className="w-80 border light p3" style={{ width: "80%" }}>
+          <div className="d-flex justify-content-between mb-3">
+            <button className="btn btn-primary">Add</button>
+
+            <input
+              type="text"
+              placeholder="Search Employees"
+              className="form-control w-50"
+            />
+          </div>
+
+          <EmployeeTable
+          employees = {employeeData.employees}
+          pagination = {employeeData.pagination}
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default EmployeeManagementApp;
