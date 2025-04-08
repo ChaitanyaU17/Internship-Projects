0609f146b47203588c53930edac00aba769e4a55
Chaitanya <104621147+ChaitanyaU17@users.noreply.github.com>
2025-04-08 23:20:33 +0530

    in employee management app import handle employee and toast container and state varible show model to show and hide form create handle add employee function

--- Employee-Management-System/frontend/src/components/EmployeeManagementApp.jsx
+++ Employee-Management-System/frontend/src/components/EmployeeManagementApp.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import EmployeeTable from "./EmployeeTable";
 import { GetAllEmployess } from "../api";
+import AddEmployee from "./AddEmployee";
+import { ToastContainer } from "react-toastify";
 
 const EmployeeManagementApp = () => {
+  const [showModal, setShowModal] = useState(false);
   const [employeeData, setEmployeeData] = useState({
     employees: [],
     pagination: {
@@ -25,13 +28,22 @@ const EmployeeManagementApp = () => {
     fetchEmployees();
   }, []);
 
+  const handleAddEmployee = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center w-100 p-3">
       <h1>Employee Management App</h1>
       <div className="w-100 d-flex justify-content-center">
         <div className="w-80 border light p3" style={{ width: "80%" }}>
           <div className="d-flex justify-content-between mb-3">
-            <button className="btn btn-primary">Add</button>
+            <button
+              className="btn btn-primary"
+              onClick={() => handleAddEmployee()}
+            >
+              Add
+            </button>
 
             <input
               type="text"
@@ -41,11 +53,19 @@ const EmployeeManagementApp = () => {
           </div>
 
           <EmployeeTable
-          employees = {employeeData.employees}
-          pagination = {employeeData.pagination}
+            fetchEmployees={fetchEmployees}
+            employees={employeeData.employees}
+            pagination={employeeData.pagination}
           />
+
+          <AddEmployee showModal={showModal} setShowModal={setShowModal} fetchEmployees={fetchEmployees} />
         </div>
       </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+      />
     </div>
   );
 };
